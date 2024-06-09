b0c957bfe78142ff3001daaabce09f3bbb0f46bf
zoecollins <107445862+zoecollins@users.noreply.github.com>
2024-06-09 18:19:11 -0500

    Update Capstone_Markdown.R

--- Capstone_Markdown.R
+++ Capstone_Markdown.R
@@ -1,10 +1,9 @@
 install.packages("tidyverse")
 install.packages("skimr")
-install.packages("janitor")
 install.packages("geosphere")
+
 library(tidyverse)
 library(skimr)
-library(janitor)
 library(geosphere)
 
 
@@ -16,25 +15,40 @@ file_2401 <- read_csv("202401-divvy-tripdata.csv")
 file_2402 <- read_csv("202402-divvy-tripdata.csv")
 file_2403 <- read_csv("202403-divvy-tripdata.csv")
 
-combined_bike_data <- bind_rows(file_2310, file_2311, file_2312, file_2401, file_2402, file_2403,.id="ride_id")
+bike_data <- bind_rows(file_2310, file_2311, file_2312, file_2401, file_2402, file_2403,.id="ride_id")
 
-head(combined_bike_data)
+head(bike_data)
+str(bike_data)
+skim(bike_data)
+spec(bike_data)
+
+#cleaned data by removing rows with missing values.
+bike_data_v2 <- drop_na(bike_data)
+str(bike_data_v2)
+skim(bike_data_v2)
+head(bike_data_v2$started_at)
 
 
+#The "start_at" and "ended_at" columns have both the date and the time aggregated together. 
+#Next we convert the "started_at" and "ended_at" columns from a character type to date-time (POSIXlt).
 
+bike_data_v2$started_at = as.POSIXlt(bike_data_v2$started_at,format="%m/%d/%Y %M:%S")
+bike_data_v2$ended_at = as.POSIXlt(bike_data_v2$ended_at,format="%m/%d/%Y %M:%S")
+bike_data_v2$ride_length = hms::as_hms(bike_data_v2$ride_length)
 
-combined_bike_data_v2 <- drop_na(combined_bike_data)
+#Now that the data types are updated, I will separate the start and end times into their own columns for better analysis.
 
-
+bike_data_v2$start_date = as.Date(bike_data_v2$started_at, format="%m/%d/%Y")
+bike_data_v2$end_date = as.Date(bike_data_v2$started_at, format="%m/%d/%Y")
+bike_data_v2$start_time = format(bike_data_v2$started_at, "%H:%M:%S")
+bike_data_v2$end_time = format(bike_data_v2$ended_at, "%H:%M:%S")
  
-distance <- distGeo(c("start_lng", "start_lat"), c("end_lng", "end_lat"))
 
+distance <- distGeo(c("start_lng", "start_lat"), c("end_lng", "end_lat"))
 combined_bike_data_v2$distance_meters <- distance
 
 
 
-skim(combined_bike_data)
-skim(combined_bike_data_v2)
 
 
 ggplot(data = combined_bike_data) + 
