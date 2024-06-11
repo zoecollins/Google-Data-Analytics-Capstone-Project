137153e60155472286cbf40e3cd456a4e6e35a68
zoecollins <107445862+zoecollins@users.noreply.github.com>
2024-06-10 19:55:53 -0500

    Update Capstone_Markdown.R

--- Capstone_Markdown.R
+++ Capstone_Markdown.R
@@ -1,3 +1,5 @@
+# Load necessary libraries
+
 install.packages("tidyverse")
 install.packages("skimr")
 install.packages("geosphere")
@@ -6,7 +8,7 @@ library(tidyverse)
 library(skimr)
 library(geosphere)
 
-
+#Reading the csv files
 file_2310 <- read_csv("202310-divvy-tripdata.csv")
 file_2311 <- read_csv("202311-divvy-tripdata.csv")
 
@@ -15,6 +17,7 @@ file_2401 <- read_csv("202401-divvy-tripdata.csv")
 file_2402 <- read_csv("202402-divvy-tripdata.csv")
 file_2403 <- read_csv("202403-divvy-tripdata.csv")
 
+#Combining the 6 months of data into one data frame to be analyzed "combined_bike_data"
 bike_data <- bind_rows(file_2310, file_2311, file_2312, file_2401, file_2402, file_2403,.id="ride_id")
 
 head(bike_data)
@@ -29,30 +32,31 @@ skim(bike_data_v2)
 head(bike_data_v2$started_at)
 
 
-#The "start_at" and "ended_at" columns have both the date and the time aggregated together. 
-#Next we convert the "started_at" and "ended_at" columns from a character type to date-time (POSIXlt).
+#converting the data type from character to datettime.
 
-bike_data_v2$started_at = as.POSIXlt(bike_data_v2$started_at,format="%m/%d/%Y %M:%S")
-bike_data_v2$ended_at = as.POSIXlt(bike_data_v2$ended_at,format="%m/%d/%Y %M:%S")
+bike_data_v2$started_at = parse_date_time(bike_data_v2$started_at, orders = "mdy HM")
+bike_data_v2$ended_at = parse_date_time(bike_data_v2$ended_at, orders = "mdy HM")
 bike_data_v2$ride_length = hms::as_hms(bike_data_v2$ride_length)
 
-#Now that the data types are updated, I will separate the start and end times into their own columns for better analysis.
-
-bike_data_v2$start_date = as.Date(bike_data_v2$started_at, format="%m/%d/%Y")
-bike_data_v2$end_date = as.Date(bike_data_v2$started_at, format="%m/%d/%Y")
-bike_data_v2$start_time = format(bike_data_v2$started_at, "%H:%M:%S")
-bike_data_v2$end_time = format(bike_data_v2$ended_at, "%H:%M:%S")
- 
-
-distance <- distGeo(c("start_lng", "start_lat"), c("end_lng", "end_lat"))
-combined_bike_data_v2$distance_meters <- distance
+#Creating separate columns for both starting and ending times and dates.
+bike_data_v2 <- bike_data_v2 %>%
+  mutate(
+    start_date = as.Date(started_at),
+    end_date = as.Date(ended_at),
+    start_time = format(started_at, "%H:%M:%S"),
+    end_time = format(ended_at, "%H:%M:%S")
+  )
 
 
+print(bike_data_v2)
+skim(bike_data_v2)
+str(bike_data_v2)
 
+#The data frame also includes starting and ending coordinates. I'm creating a column to calculate the distance made during each trip using distGeo(). 
+#This function outputs in meters which I will convert to miles.
+bike_data_v2 <- bike_data_v2 %>%
+  mutate (
+    distance_meters = distGeo(cbind(start_lng, start_lat), cbind(end_lng, end_lat)),
+    distance_miles = distance_meters / 1609.344
+    )
 
-
-ggplot(data = combined_bike_data) + 
-  geom_bar(mapping = aes(x = rideable_type)) + facet_wrap(~member_casual)
-
-ggplot(data = combined_bike_data) +
-  geom_point(mapping = aes(x= member_casual, y= start_station_name))
